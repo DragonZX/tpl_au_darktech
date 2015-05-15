d7c63813ba35bbbc15885b4e4948008651496dc4
DragonZX <DragonZX@yandex.ru>
2015-05-15 20:26:09 +0300

    Creation begins

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
-<?php
-#Initial
+﻿<?php
+#Initial Block
+date_default_timezone_set('America/Los_Angeles');
+$script_tz = date_default_timezone_get();
+
+if (strcmp($script_tz, ini_get('date.timezone'))){
+    echo 'переменная зона скрипта отличается от заданной в INI-файле.';
+} else {
+    echo 'переменная зоны скрипта и настройки INI-файла совпадают.';
+}
+?>
+
 <html>
 <head>
-<title></title>
+<title>DarkTech Template</title>
 </head>
 <body>
-<footer>Copyright &copy 2009-2015 Amvis United Company Limited. GNU/GPL v3</footer>
+<footer>Copyright &copy; 2009-2015 Amvis United Company Limited. <a href="https://gnu.org/licenses/gpl-3.0.txt">GNU/GPL v3</a></footer>
 </body>
 <html>
-?>
